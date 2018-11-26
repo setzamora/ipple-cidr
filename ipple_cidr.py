4629ae125fa7e90188b1455702155b1c0cd63e65
setzamora <joset.zamora@outlook.com>
2018-11-26 13:43:47 +1100

    Follow snake case naming

--- ipple_cidr.py
+++ ipple_cidr.py
@@ -1,24 +1,28 @@
 import csv
 import re
 import sys
+from prettytable import PrettyTable
 
 def read_csv(*argv):
     for filename in argv[0]:
         filename = filename.strip()
+    table = PrettyTable(['FROM', 'TO', 'CIDR'])
+    table.align = 'r'
     csv_reader = csv.reader(open(filename))
     for row in csv_reader:
         mask_bit = get_cidr_ip_range(row[0], row[1])
-        print(row[0] + ' to ' + row[1] + ' => ' + row[0] + '/' + str(mask_bit))
+        table.add_row([row[0], row[1], row[0] + '/' + str(mask_bit)])
+    print(table)
 
 
-def get_cidr_ip_range(ip1, ip2):
+def get_cidr_ip_range(ip_address_1, ip_address_2):
     range_dictionary = {0: 0, 1: 1, 3: 2, 7: 3, 15: 4, 31: 5, 63: 6, 127: 7, 255: 8}
     cl = [0, 0, 0, 0]
 
-    a1, a2 = split_ip_address(ip1, ip2)
+    address_1, address_2 = split_ip_address(ip_address_1, ip_address_2)
     for i in range(len(cl)):
-        if difference_list(a2, a1, i) in range_dictionary:
-            cl[i] = 8 - range_dictionary[difference_list(a2, a1, i)]
+        if difference_list(address_2, address_1, i) in range_dictionary:
+            cl[i] = 8 - range_dictionary[difference_list(address_2, address_1, i)]
         else:
             print('Error in converting to CIDR notation')
             sys.exit()
@@ -46,17 +50,17 @@ def difference_list(la, lb, idx):
     return int(la[idx]) - int(lb[idx])
 
 
-def split_ip_address(ip1, ip2):
-    if not check_ip_address(ip1):
-        print('First IP address is invalid ' + ip1)
+def split_ip_address(ip_address_1, ip_address_2):
+    if not check_ip_address(ip_address_1):
+        print('First IP address is invalid ' + ip_address_1)
 
-    if not check_ip_address(ip2):
-        print('Second IP address is invalid ' + ip2)
+    if not check_ip_address(ip_address_2):
+        print('Second IP address is invalid ' + ip_address_2)
 
-    li1 = ip1.split('.')
-    li2 = ip2.split('.')
+    li_1 = ip_address_1.split('.')
+    li_2 = ip_address_2.split('.')
 
-    return (li1, li2)
+    return (li_1, li_2)
 
 
 def check_ip_address(ip):
